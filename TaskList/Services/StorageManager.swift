cf292dbd90c0eae0197b52870938fbef367fcc15
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-08-22 11:23:22 +0300

    Added StorageManager

---
+++ TaskList/Services/StorageManager.swift
@@ -0,0 +1,37 @@
+//
+//  StorageManager.swift
+//  TaskList
+//
+//  Created by Дмитрий Дуров on 22.08.2022.
+//
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskList")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private init() {}
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
